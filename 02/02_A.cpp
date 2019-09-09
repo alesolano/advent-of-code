1c233a97ec44b19525d3e10aec195dd7a9b11739
Alejandro Solano <asolanopf@gmail.com>
2019-09-09 18:51:07 +0200

    working 02A. super improvable

--- 02/02_A.cpp
+++ 02/02_A.cpp
@@ -1,8 +1,17 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <unordered_set>
 #include <utility> // std::pair
+#include <memory> // std::unique_ptr
 
 using uset_char = std::unordered_set<char>;
 
@@ -25,9 +34,7 @@ struct IsTwoThreePair
     IsTwoThreePair(const NTwoThreePair& n)
     {
         is_three = n.n_threes > 0;
-        
-        // THIS CONDITION IS WRONG
-        is_two = (n.n_twos != n.n_threes) and (n.n_twos > 0);
+        is_two = n.n_twos > 0;
     }
 };
 
@@ -41,22 +48,33 @@ IsTwoThreePair check_repeated(std::string& key)
 {
     NTwoThreePair n_result;
     
-    uset_char chars1;
-    uset_char chars2;
+    std::unique_ptr<uset_char> chars1(new uset_char);
+    std::unique_ptr<uset_char> chars2(new uset_char);
+    std::unique_ptr<uset_char> chars3(new uset_char);
     std::pair<uset_char::iterator, bool> is_new_char_pair;
     
     // Linear O(m), m being the size of the string
     for (char& c : key)
     {
-        // Average: constant
-        is_new_char_pair = chars1.insert(c);
+        // Insert with average constant complexity
+        is_new_char_pair = chars1->insert(c);
+        // If is not new, then is repeated more than once
         if (!is_new_char_pair.second)
         {
-            // Average: constant
-            is_new_char_pair = chars2.insert(c);
+            // Insert with average constant complexity
+            is_new_char_pair = chars2->insert(c);
+            // If is not new, then is repeated more than twice
             if (!is_new_char_pair.second)
             {
-                n_result.n_threes += 1;
+                // Insert with average constant complexity
+                is_new_char_pair = chars3->insert(c);
+                // If is not new, then is repeated more than three times
+                if (!is_new_char_pair.second) n_result.n_threes -= 1; // It was not a three!
+                else
+                {
+                    n_result.n_threes += 1; // It's a three!
+                    n_result.n_twos -= 1;   // It was not a two!
+                }
             }
             else n_result.n_twos += 1;
         }
@@ -75,12 +93,12 @@ int main()
     std::string line;
     if (ifile.is_open())
     {
-        // Linear O(n) where n is the number of lines
+        // Overall algorithm: Linear O(n*m)
+        // while: Linear O(n) where n is the number of lines
         while(ifile >> line)
         {
+            // check_repeated: Linear O(m), m being the size of the line
             update_n(n_result, check_repeated(line));
-            std::cout << line << std::endl;
-            std::cout << n_result.n_twos << " " << n_result.n_threes << std::endl;
         }
         ifile.close();
     }
